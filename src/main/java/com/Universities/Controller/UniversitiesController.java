b6f5c29b5cb69185cbeb33a438c9a1b8b298423b
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-08-12 12:34:31 +0530

    All CRUD operation to access data are integrated

--- src/main/java/com/Universities/Controller/UniversitiesController.java
+++ src/main/java/com/Universities/Controller/UniversitiesController.java
@@ -1,7 +1,12 @@
 package com.Universities.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +18,28 @@ public class UniversitiesController {
 	@Autowired
 	private UniversitiesServer uniService;
 	
+	@GetMapping("/University")
+	public List<UniversitiesModel> getFindAll()
+	{
+		return uniService.findAll();
+	}
+	
+	@GetMapping("/University/{Id}")
+	public UniversitiesModel getFindOne(@PathVariable int Id) 
+	{
+		return uniService.findOne(Id);
+	}
+	
 	@PostMapping("/University")
 	public void getSaveOneDat(@RequestBody UniversitiesModel uniMod) 
 	{
 		uniService.SaveOneData(uniMod);
 	}
+	
+	@PutMapping("/University/{Id}")
+	public List<UniversitiesModel> getUpdateOne(@PathVariable int Id ,@RequestBody  UniversitiesModel uniMod)
+	{
+		return uniService.updateOne(Id, uniMod);
+	}
+	
 }
--- src/main/java/com/Universities/Server/UniversitiesServer.java
+++ src/main/java/com/Universities/Server/UniversitiesServer.java
@@ -1,5 +1,9 @@
 package com.Universities.Server;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +18,43 @@ public class UniversitiesServer {
 	@Autowired
 	private UniversitiesRepository uniRepo;
 
+	public List<UniversitiesModel> findAll()
+	{
+		List<UniversitiesModel> userData = new ArrayList<>();
+		uniRepo.findAll().forEach(userData::add);
+		return userData;
+	}
+	
+	public UniversitiesModel findOne(int Id)
+	{
+		return uniRepo.findById(Id).get();
+	}
+	
 	public void SaveOneData(UniversitiesModel uniMod) 
 	{
 		uniRepo.save(uniMod);
+	}
+	
+	public List<UniversitiesModel> updateOne(int Id , UniversitiesModel uniMod)
+	{
+//		uniRepo.deleteById(Id);
+//		uniRepo.save(uniMod);
+//		return findAll();
+		
+		if(uniRepo.findById(Id).isPresent())
+		{
+			UniversitiesModel checking = uniRepo.findById(Id).get();
+			checking.setUniaddress( uniMod.getUniaddress() );
+			checking.setUniname( uniMod.getUniname() );
+			checking.setUnipasspercentage( uniMod.getUnipasspercentage() );
+			
+			uniRepo.save(checking);
+		}
+		return findAll();
+	}
+	
+	public void deletONe(int Id) 
+	{
+		uniRepo.deleteById(Id);
 	}
 }
